3336f23899713477a19a571447fd6a53f882dc34
Steven Shaw <steven@steshaw.org>
2017-03-23 15:08:51 +1000

    Try the HSpec WAI json quasi-quoter

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import Server (app)
+
 import Test.Hspec
 import Test.Hspec.Wai
-import Test.Hspec.Wai.JSON
+import Test.Hspec.Wai.JSON (json)
 
 main :: IO ()
 main = hspec spec
@@ -17,5 +19,23 @@ spec = with (return app) $ do
         it "responds with 200" $ do
             get "/users" `shouldRespondWith` 200
         it "responds with [User]" $ do
-            let users = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"},{\"userId\":3,\"userFirstName\":\"Fred\",\"userLastName\":\"Flintstone\"}]"
+            let users = [json|
+  [
+    {
+      "userId": 1,
+      "userFirstName":"Isaac",
+      "userLastName":"Newton"
+    },
+    {
+      "userId": 2,
+      "userFirstName": "Albert",
+      "userLastName": "Einstein"
+    },
+    {
+      "userId": 3,
+      "userFirstName": "Fred",
+      "userLastName": "Flintstone"
+    }
+  ]
+|]
             get "/users" `shouldRespondWith` users
